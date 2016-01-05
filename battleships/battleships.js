5aabddf195e4cc3255cf4f6a849233ed6ae0232c
Tim Man <tim@lokku.com>
2016-01-05 21:02:47 +0000

    more battleships functionality

--- battleships/battleships.js
+++ battleships/battleships.js
@@ -2,11 +2,16 @@
 
 var clone = require('clone');
 var log = require('loglevel');
-var prompt = require('prompt');
+var readline = require('readline');
 
 /*
  * class Game
  */
+
+var OCEAN = '~';
+var MISS = 'x';
+var HIT = '*';
+
 function Game (size) {
   // default variables in ES2015. TODO use babel
   this.gridSize = typeof size !== 'undefined' ? size : 10; // default 10
@@ -67,31 +72,105 @@ Game.prototype.placeAllShips = function () {
   while (!this.placeShip('destroyer')) {};
 }
 
+var alpha = {a:0, b:1, c:2, d:3, e:4, f:5, g:6, h:7, i:8, j:9};
 function gameCoordsToXY (string) {
+  // really doesn't handle larger numbers than 99
+  if (string.length < 2 || string.length > 3) {
+    return;
+  }
 
+  //TODO TEST THIS FUNCTION FOR A15
+  var r = new Object();
+  string.split('').map(function(v, i){
+    var key = i === 0 ? 'x' : 'y';
+    if (key === 'x') {
+      v = alpha[v.toLowerCase()];
+    } else {
+      v -= 1; 
+    }
+    r[key] = v;
+  });
+  log.debug(r);
+  return r;
 }
 
-Game.prototype.fireAt = function (x,y) {
-  this._grid[y][x] = 'x';
-  return 0;
+/*
+ * Returns 1 if valid, else undef
+ * 
+ * Expects { x: INT, y: INT }
+ */
+Game.prototype.validXY = function (r) {
+  // not an object
+  if (!r) {
+    return;
+  }
+
+  if (!Number(r.x) || !Number(r.y)) {
+    return;
+  }
+
+  if (r.x > this.gridSize || r.y > this.gridSize) {
+    return;
+  }
+
+  return 1;
+}
+
+Game.prototype.fireAt = function (coords) {
+  var r = gameCoordsToXY(coords);
+  if (!this.validXY(r)) {
+    log.info("that's not a valid coordinate");
+    return;
+  }
+
+  switch (this._grid[r.y][r.x]) {
+    case OCEAN:
+    case MISS: 
+      this._grid[r.y][r.x] = MISS;
+      return 0;
+    default:
+      this._grid[r.y][r.x] = HIT;
+      return 1;
+  }
 }
 
 Game.prototype.printGrid = function () {
   log.info(this._grid);
+  log.info('\n\n\n\n\n');
 }
-// class GAME
+// end class GAME
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function run (g) {
+  rl.question("where them ships at? ", function(coords) {
+    log.info('\x1B[2J');
+    if (coords === 'exit') {
+      return rl.close();
+    }
+    var hit = g.fireAt(coords);
+    if (hit) {
+      log.info('HIT\n');
+    }
+    g.printGrid();
+    run(g);
+  });
+}
 
 log.setLevel('info');
+log.info('\x1B[2J');
 
-log.info('welcome to battleships')
-log.info('new game')
+rl.question('welcome to battleships\n\n PRESS ENTER', function() {
+  log.info('\x1B[2J');
+  log.info('new game')
+  log.info('\n\n\n\n\n');
 
-var g = new Game(10);
-g.placeAllShips();
-g.printGrid();
-
-prompt.start();
-prompt.get(['x','y'], function (err,result) {
-  g.fireAt(result.x, result.y);
+  var g = new Game(10);
+  g.placeAllShips();
   g.printGrid();
+  run(g);
 });
+
